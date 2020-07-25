1de33036e477af8e5449d68c8acabeaf68d84346
Amar Prakash Pandey <amar.om1994@gmail.com>
2020-07-25 22:13:54 +0530

    Refacotred the model class and impl builder pattern

--- src/main/java/me/amarpandey/model/Message.java
+++ src/main/java/me/amarpandey/model/Message.java
@@ -6,31 +6,33 @@ public class Message {
 	private String text;
 	private Type type;
 
-	public String getFrom() {
-		return from;
-	}
-
-	public void setFrom(String from) {
-		this.from = from;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public Type getType() {
-		return type;
-	}
-
-	public void setMessageType(Type type) {
-		this.type = type;
+	public Message(Builder builder) {
+		this.from = builder.from;
+		this.text = builder.text;
+		this.type = builder.type;
 	}
 
 	public enum Type {
 		CHAT, JOIN, LEAVE
+	}
+
+	public static class Builder {
+		private String from;
+		private String text;
+		private Type type;
+
+		public Builder(String from, String text) {
+			this.from = from;
+			this.text = text;
+		}
+
+		public Builder messageType(Type type) {
+			this.type = type;
+			return this;
+		}
+
+		public Message build() {
+			return new Message(this);
+		}
 	}
 }
