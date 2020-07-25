51943e6d050ebfdb4a61e3dca12fc3f192f036fb
Amar Prakash Pandey <amar.om1994@gmail.com>
2020-07-25 19:44:55 +0530

    Some basic refactoring

--- src/main/java/me/amarpandey/ChatRoomsApplication.java
+++ src/main/java/me/amarpandey/ChatRoomsApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebsocketsApplication {
-
+public class ChatRoomsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebsocketsApplication.class, args);
+		SpringApplication.run(ChatRoomsApplication.class, args);
 	}
 }
